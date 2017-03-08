b9e8d7e9b57f062b9b39c9c20f986cac558a6485
Florian Schütt <florian.schuett@pmagentur.com>
2017-03-08 16:22:03 +0100

    download from ftp and import articles works (manual cron-call)

--- public/custom/plugins/PmImport/PmImport.php
+++ public/custom/plugins/PmImport/PmImport.php
@@ -31,39 +31,39 @@ class PmImport extends Plugin {
     }
 
     public function ImportFTPDataAction(\Shopware_Components_Cron_CronJob $job)
-	{	
-    	$list = array (
-		    array("test")
-		);
+	{
+	    //download csv and import the articles
+        $local_file = 'files/import_cron/ImportArticleTest.shopware.csv';
+        $server_file = 'ImportArticleTest.shopware.csv';
+        $host = "192.168.178.26";
+        $port = "8080";
 
-		$fp = fopen('files/export/orders/export.csv', 'a');
-		foreach ($list as $fields) {
-		    fputcsv($fp, $fields, ";", '"');
-		}
-		fclose($fp);
+        $conn_id = ftp_connect($host);
+        //set the env with 'ftp_password=pw' and 'export ftp_password' in the docker machine
+        $login_result = ftp_login($conn_id, "Florian", getenv("ftp_password"));
+        if ($login_result) {
+            echo "login success\n";
+            //passive mode
+            ftp_pasv($conn_id, true);
+            ftp_chdir($conn_id, "home");
+            ftp_chdir($conn_id, "shopware");
 
-		return 'Yes its running!';
+            if (ftp_get($conn_id, $local_file, $server_file, FTP_BINARY)) {
+                echo "$local_file saved\n";
+                $response = file_get_contents("http://$host:$port/backend/SwagImportExportCron/cron");
+
+            }
+            else {
+                $response = "error saving file!";
+            }
+        }
+        else {
+            $response = "login failed!";
+        }
+
+
+        echo $response."\n";
+        ftp_close($conn_id);
+		return $response;
 	}
-
-    public function ftp() {
-    	// Variablen definieren
-		$local_file = 'test123.csv';
-		$server_file = 'test.csv';
-
-		// Verbindung aufbauen
-		$conn_id = ftp_connect("localhost");
-
-		// Login mit Benutzername und Passwort
-		$login_result = ftp_login($conn_id, "PM", "pm");
-
-		// Versuche $server_file herunterzuladen und in $local_file zu speichern
-		if (ftp_get($conn_id, $local_file, $server_file, FTP_BINARY)) {
-		    echo "$local_file wurde erfolgreich geschrieben\n";
-		} else {
-		    echo "Ein Fehler ist aufgetreten\n";
-		}
-
-		// Verbindung schließen
-		ftp_close($conn_id);
-    }
 }
